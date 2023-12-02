c9442f5f3e0ef99e09edd8bb7153b61f2420847a
byronboots <bootsbyron@gmail.com>
2023-12-02 01:57:06 -0800

    clean up comments

--- Day1/day1solution.py
+++ Day1/day1solution.py
@@ -5,16 +5,16 @@ calibration_values = open('/Users/byronbootspmbp/GitHub_Projects/adventofcode202
 
 # set a sum variable to keep track of total calculated sum from the calibration values
 sum1 = 0
-# iterate through each line of calibration values taking each line as its own value
+# iterate through each line of calibration_values taking each line as its own value
 for value in calibration_values:
     # initialize empty array to store found digits
     digits = []
     # iterate through the full string line to find the first and last digits
     for number, digit in enumerate(value):
-        # if the character we're on isnumeric, check if the first digit is populated yet. If it is, update the last_digit value
+        # if the character we're on isnumeric, if it is add it to our digits array
         if digit.isdigit():
             digits.append(digit)
-    # if our string had only one digit, we use the first digit twice (e.g treb7uchet = 77)
+    # concatenate and then sum our first and last 
     sum1 += int(digits[0]+digits[-1])
         
 print(sum1)
@@ -23,22 +23,25 @@ print(sum1)
 
 ## Part 2
 
+# Open our input list and read into our variable
 calibration_values = open('/Users/byronbootspmbp/GitHub_Projects/adventofcode2023/Day1/input1.txt', 'r')
+
 # set a sum variable to keep track of total calculated sum from the calibration values
 sum2 = 0
-# iterate through each line of calibration values taking each line as its own value
+# iterate through each line of calibrationvalues taking each line as its own value
 for value in calibration_values:
     # initialize empty array to store found digits
     digits = []
     # iterate through the full string line to find the first and last digits
     for number, digit1 in enumerate(value):
-        # if the character we're on isnumeric, check if the first digit is populated yet. If it is, update the last_digit value
+        # if the character we're on isnumeric, if it is add it to our digits array
         if digit1.isdigit():
             digits.append(digit1)
+        # if we found the word representation of a digit add the numeric digit as a string to our digits array
         for string, digit2 in enumerate(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
             if value[number:].startswith(digit2):
                 digits.append(str(string+1))
-    # if our string had only one digit, we use the first digit twice (e.g treb7uchet = 77)
+    # concatenate and then sum our first and last digits
     sum2 += int(digits[0]+digits[-1])
     
 print(sum2)
